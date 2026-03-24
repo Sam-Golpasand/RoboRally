176b5aa136dbc9363bb4063dd4bd2dcc96f9fe94
Sam Golpasand <samgolpasand45@gmail.com>
2026-03-24 10:14:25 +0100

    implemented some small requirments

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -78,7 +78,7 @@ public class AppController implements Observer {
                 }
             }
 
-            // TODO A6b: Use a user dialog here (similar to the one above
+            // DONE A6b: Use a user dialog here (similar to the one above
             //     for player number) which lets the user select one of the
             //     available boards, and then create the chosen board using
             //     the BoardFactory (instead of creating an empty board).
