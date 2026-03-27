518a916b5b8fc8723830b351bb2c3b5440d9aac9
Sam Golpasand <samgolpasand45@gmail.com>
2026-03-27 10:12:24 +0100

    made TODO to DONE

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -268,7 +268,7 @@ public class Board extends Subject {
 
 
 
-        // TODO A6c: changed the status so that it shows the phase, the current player, and the current register
+        // DONE A6c: changed the status so that it shows the phase, the current player, and the current register
         //     and you can remove the move count status message message and the corresponding counter again
         // TODO A6e: add something to the status message, when a player has won the game
         return "Player = " + getCurrentPlayer().getName() + "Phase: " + phase.name().toLowerCase() ;
